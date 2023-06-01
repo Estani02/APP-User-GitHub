2b6d8ca87cac36d218e534966c0f23c4d3647f62
Estani02 <estaniolmedo@gmail.com>
2023-06-01 13:08:44 -0300

    added media in container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import SearchBar from "@/components/search_bar"
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center container">
         <SearchBar />
         {/* Hola mundo!! */}
       </div>
--- src/components/search_bar/index.tsx
+++ src/components/search_bar/index.tsx
@@ -4,13 +4,15 @@ import React from 'react'
 const SearchBar = () => {
   return (
     <form className='p-2 bg-[#202A49] rounded-lg flex flex-row items-center justify-center w-fit gap-2'>
-      <SearchIcon className='fill-[#1758B4]' />
+      <span>
+        <SearchIcon className='fill-[#1758B4]' />
+      </span>
       <input
         type="text"
-        className='outline-none bg-[#202A49] text-white'
+        className='outline-none bg-transparent text-white flex-1 md:w-[400px] lg:w-[500px]'
         placeholder='Search GitHub username...'
       />
-      <button type='submit' className='rounded-lg bg-[#0079FF] text-white p-3'>
+      <button type='submit' className='rounded-lg bg-[#0079FF] text-white p-3 hover:bg-[#0077ffc1] transition-color duration-300 font-semibold'>
         Search
       </button>
     </form>
