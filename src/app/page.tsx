ade491fcd7d122162b415cd64ac8f6ef26963b81
Estani02 <estaniolmedo@gmail.com>
2023-06-02 17:40:30 -0300

    added card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
+import Card from "@/components/card"
 import SearchBar from "@/components/search_bar"
 
 export default function Home() {
   return (
-    <>
-      <div className="flex flex-col justify-center items-center container">
-        <SearchBar />
-        {/* Hola mundo!! */}
-      </div>
-    </>
+    <div className="flex flex-col justify-center items-center gap-6 mt-6 mb-6 w-full px-6">
+      <SearchBar />
+      <Card />
+    </div>
   )
 }
