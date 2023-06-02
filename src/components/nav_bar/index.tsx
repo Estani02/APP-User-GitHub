fbae94b4a5158dcd260971ee4dca429ec237004b
Estani02 <estaniolmedo@gmail.com>
2023-06-02 17:42:11 -0300

    fixed

--- src/components/nav_bar/index.tsx
+++ src/components/nav_bar/index.tsx
@@ -1,9 +1,14 @@
+import SunIcon from '@/icons/sun'
 import React from 'react'
 
 const NavBar = () => {
   return (
-    <nav className='flex justify-center items-center py-5 bg-blue-950 text-blue-200'>
-      NavBar
+    <nav className='flex justify-between items-center p-3 md:py-5 px-3 bg-blue-950 text-blue-200'>
+      <h1 className='font-semibold'>devfinder</h1>
+      <button className='flex flex-row items-center font-medium gap-2'>
+        LIGHT
+        <SunIcon />
+      </button>
     </nav>
   )
 }
--- src/components/search_bar/index.tsx
+++ src/components/search_bar/index.tsx
@@ -1,18 +1,18 @@
-import SearchIcon from '@/app/icons/serarch'
+import SearchIcon from '@/icons/serarch'
 import React from 'react'
 
 const SearchBar = () => {
   return (
-    <form className='p-2 bg-[#202A49] rounded-lg flex flex-row items-center justify-center w-fit gap-2'>
+    <form className='p-2 bg-[#202A49] rounded-lg flex flex-row items-center justify-center gap-2 w-full md:max-w-[700px]'>
       <span>
         <SearchIcon className='fill-[#1758B4]' />
       </span>
       <input
         type="text"
-        className='outline-none bg-transparent text-white flex-1 md:w-[400px] lg:w-[500px]'
+        className='outline-none bg-transparent text-white flex-1 h-full'
         placeholder='Search GitHub username...'
       />
-      <button type='submit' className='rounded-lg bg-[#0079FF] text-white p-3 hover:bg-[#0077ffc1] transition-color duration-300 font-semibold'>
+      <button type='submit' className='rounded-lg bg-[#0079FF] text-white p-1 md:p-2 lg:p-3 hover:bg-[#0077ffc1] transition-color duration-300 font-semibold'>
         Search
       </button>
     </form>
