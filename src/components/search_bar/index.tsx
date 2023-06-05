c0d097e14ca62b6a556214c51a9a1396ecc64470
Estani02 <estaniolmedo@gmail.com>
2023-06-05 11:52:27 -0300

    search input functionality

--- src/components/search_bar/index.tsx
+++ src/components/search_bar/index.tsx
@@ -1,14 +1,35 @@
+'use client';
 import SearchIcon from '@/icons/serarch'
-import React from 'react'
+import React, { ReactEventHandler, useState } from 'react'
 
-const SearchBar = () => {
+interface Props {
+  getUsers: (username: string) => Promise<void>
+}
+
+const SearchBar = ({ getUsers }: Props) => {
+
+  const [input, setInput] = useState('')
+
+  const handleSubmit = async (e: React.FocusEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    await getUsers(input)
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value)
+  }
+
   return (
-    <form className='p-2 bg-[#202A49] rounded-lg flex flex-row items-center justify-center gap-2 w-full md:max-w-[700px]'>
+    <form onSubmit={handleSubmit} className='p-2 bg-[#202A49] rounded-lg flex flex-row items-center justify-center gap-2 md:h-[3.5rem] lg:h--[4.5rem] w-full md:max-w-[700px] mt-6'>
       <span>
         <SearchIcon className='fill-[#1758B4]' />
       </span>
       <input
+        onChange={handleChange}
         type="text"
+        value={input}
+        name='username'
         className='outline-none bg-transparent text-white flex-1 h-full'
         placeholder='Search GitHub username...'
       />
