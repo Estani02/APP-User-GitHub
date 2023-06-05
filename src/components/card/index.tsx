0b841facb6e1d8242a93eee825f609d0e623548f
Estani02 <estaniolmedo@gmail.com>
2023-06-05 16:46:19 -0300

    added next image component and fixed styles

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,9 +1,9 @@
-/* eslint-disable @next/next/no-img-element */
 import CityIcon from '@/icons/city'
 import LinkIcon from '@/icons/link'
 import LocationIcon from '@/icons/location'
 import TwitterIcon from '@/icons/twitter'
 import { DataUser } from '@/interfaces/user'
+import Image from 'next/image'
 import React, { use } from 'react'
 
 interface PropsUser {
@@ -13,10 +13,10 @@ interface PropsUser {
 const Card = ({ user }: PropsUser) => {
   return (
     <section className='bg-[#202A49] flex flex-col md:flex-row gap-5 px-5 py-5 md:p-10 rounded-lg w-full md:max-w-[700px] lg:max-w-[850px] mb-28'>
-      <img src={user.avatar_url} alt="Avatar" className='rounded-[50%] h-20 w-20 hidden md:block' />
+      <Image src={user.avatar_url} width={80} height={80} alt="Avatar" className='rounded-[50%] h-20 w-20 hidden md:block' />
       <div className='md:flex md:flex-col gap-6'>
         <article className='flex flex-row gap-3'>
-          <img src="https://i.pravatar.cc/150?u=dapelu" alt="Avatar de dapelu" className='rounded-[50%] h-auto w-20 md:hidden' />
+          <Image src={user.avatar_url} width={80} height={80} alt="Avatar de dapelu" className='rounded-[50%] h-auto w-20 md:hidden' />
           <div className='flex flex-col text-blue-200'>
             <h3>{user.name}</h3>
             <p className='text-sky-600 text-xs md:text-sm'>@{user.login}</p>
@@ -30,7 +30,7 @@ const Card = ({ user }: PropsUser) => {
           </div>
         </article>
         <article className='flex flex-col justify-center items-center'>
-          <p className='text-[#8C93A0]'>
+          <p className='text-[#8C93A0] mt-4'>
             {user.bio}
           </p>
           <div className='flex flex-row justify-center gap-10 mt-7 py-6 px-4 rounded-lg bg-[#161C30] w-full'>
@@ -55,11 +55,11 @@ const Card = ({ user }: PropsUser) => {
           </div>
           <div className='flex flex-row items-center gap-2'>
             <LinkIcon className='fill-white' />
-            <a href={user.html_url} target='_blank' className='mr-2 text-white'>{user.html_url}</a>
+            <a href={user.html_url} target='_blank' className='mr-2 truncate text-white'>{user.html_url}</a>
           </div>
           <div className='flex flex-row items-center gap-2'>
             <TwitterIcon className='fill-white' />
-            <span className={`mr-2 ${user.twitter_username ? 'text-white' : 'text-gray-400'}`}>{user.twitter_username ? user.twitter_username : "Not Available"}</span>
+            <span className={`mr-2 truncate ${user.twitter_username ? 'text-white' : 'text-gray-400'}`}>{user.twitter_username ? user.twitter_username : "Not Available"}</span>
           </div>
           <div className='flex flex-row items-center gap-2'>
             <CityIcon className='fill-white' />
