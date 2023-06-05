50125e938ecae41c79cdf7e904f4cb1096502a78
Estani02 <estaniolmedo@gmail.com>
2023-06-05 16:44:47 -0300

    handle request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,16 @@ import { useState } from "react"
 
 export default function Home() {
 
-  const [user, setUser] = useState<DataUser | null>(null)
+  const [user, setUser] = useState<DataUser | null | undefined>(undefined)
+  const [username, setUsername] = useState<string>('')
 
   const getUsers = async (username: string) => {
     const res = await fetch(`https://api.github.com/users/${username}`)
+    if (!res.ok) {
+      setUser(null)
+      setUsername(username)
+      return
+    }
     const data = await res.json()
     setUser(data)
   }
@@ -18,8 +24,12 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center gap-6 w-full px-6 bg-[#161C30] text-sm md:text-base">
       <SearchBar getUsers={getUsers} />
-      {user && <Card user={user} />}
-
+      {user ?
+        <Card user={user} /> : user === null ?
+          <div className=" mt-20 flex justify-center items-center text-white font-bold text-xl md:text-2xl">
+            Username {`"${username}"`} not found
+          </div> : <></>
+      }
     </div>
   )
 }
