56f3135628fa991c7eaf15f8ac756a3c46c73bc0
Estani02 <estaniolmedo@gmail.com>
2023-06-07 13:32:36 -0300

    added dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className='dark'>
       <body className={`${spaceGrotesk.className} bg-blue-50 dark:bg-[#161C30] text-sm md:text-base`}>
         <NavBar />
         {children}
--- src/components/nav_bar/index.tsx
+++ src/components/nav_bar/index.tsx
@@ -1,13 +1,45 @@
+'use client';
+import MoonIcon from '@/icons/moon';
 import SunIcon from '@/icons/sun'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+// const initialThemeState = localStorage.getItem('theme') as 'light' | 'dark' || window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+
+const initialThemeState = () => {
+  if (typeof window !== 'undefined') {
+    if (localStorage.getItem('theme')) {
+      return localStorage.getItem('theme') as 'light' | 'dark'
+    } else {
+      return window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light'
+    }
+  }
+  return 'dark'
+}
 
 const NavBar = () => {
+  const [theme, setTheme] = useState<'light' | 'dark'>(initialThemeState)
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add("dark")
+    } else {
+      document.documentElement.classList.remove("dark")
+    }
+    localStorage.setItem('theme', theme)
+  }, [theme])
+
+  const handleTheme = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+  }
+
   return (
-    <nav className='flex justify-between items-center p-3 md:py-5 px-3 text-blue-950 dark:bg-blue-950 dark:text-blue-200 text-base md:text-xl lg:text-2xl font-semibold md:font-bold'>
+    <nav className='flex justify-between items-center p-3 md:py-5 px-3 text-blue-950 dark:bg-blue-950 dark:text-blue-200 text-base md:text-xl font-semibold md:font-bold'>
       <h1 className='uppercase'>devfinder</h1>
-      <button className='flex flex-row items-center gap-2'>
-        LIGHT
-        <SunIcon />
+      <button onClick={handleTheme} className='flex flex-row items-center gap-2'>
+        {theme === 'light' ? `LIGHT` : `DARK`}
+        {theme === 'light' ? <MoonIcon /> : <SunIcon />}
       </button>
     </nav>
   )
