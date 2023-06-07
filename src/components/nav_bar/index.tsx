58cbed2952af79c2d5c56b7b30da39e3310b0752
Estani02 <estaniolmedo@gmail.com>
2023-06-07 17:17:38 -0300

    fix localstorage & added mode transition

--- src/components/nav_bar/index.tsx
+++ src/components/nav_bar/index.tsx
@@ -3,23 +3,14 @@ import MoonIcon from '@/icons/moon';
 import SunIcon from '@/icons/sun'
 import React, { useEffect, useState } from 'react'
 
-// const initialThemeState = localStorage.getItem('theme') as 'light' | 'dark' || window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-
-const initialThemeState = () => {
-  if (typeof window !== 'undefined') {
-    if (localStorage.getItem('theme')) {
-      return localStorage.getItem('theme') as 'light' | 'dark'
-    } else {
-      return window.matchMedia('(prefers-color-scheme: dark)').matches
-        ? 'dark'
-        : 'light'
-    }
-  }
-  return 'dark'
-}
 
 const NavBar = () => {
-  const [theme, setTheme] = useState<'light' | 'dark'>(initialThemeState)
+  const [theme, setTheme] = useState<'light' | 'dark' | string>('dark')
+
+  useEffect(() => {
+    const themeLS = localStorage.getItem('theme') ?? 'dark'
+    setTheme(themeLS)
+  }, [])
 
   useEffect(() => {
     if (theme === 'dark') {
@@ -35,7 +26,7 @@ const NavBar = () => {
   }
 
   return (
-    <nav className='flex justify-between items-center p-3 md:py-5 px-3 text-blue-950 dark:bg-blue-950 dark:text-blue-200 text-base md:text-xl font-semibold md:font-bold'>
+    <nav className='flex justify-between items-center p-3 md:py-5 px-3 text-blue-950 dark:bg-blue-950 dark:text-blue-200 text-base md:text-xl font-semibold md:font-bold transition-colors duration-300'>
       <h1 className='uppercase'>devfinder</h1>
       <button onClick={handleTheme} className='flex flex-row items-center gap-2'>
         {theme === 'light' ? `LIGHT` : `DARK`}
